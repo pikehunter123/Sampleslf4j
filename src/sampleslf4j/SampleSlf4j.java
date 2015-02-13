60e159e79526392d61613875fb5bc2c85bc76ea9
kononov446 <kononov446@LPR182.main.rosevrobank.ru>
2015-02-13 17:34:38 +0400

    lets go

---
+++ src/sampleslf4j/SampleSlf4j.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sampleslf4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author kononov446
+ */
+public class SampleSlf4j {
+final static Logger logger = LoggerFactory.getLogger(SampleSlf4j.class);
+    /**
+     * @param args the command line arguments
+     */
+   public static void main(String[] args) {
+    
+    
+    logger.debug("debug message");
+    logger.info("info message");
+    logger.warn("warn message");
+    logger.error("error message");
+  }
+
+    
+}
